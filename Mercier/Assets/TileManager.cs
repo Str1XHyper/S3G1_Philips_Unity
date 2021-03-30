20c9e9e93f34ed5b1816a79da64a1d1af73c4726
Luc Somers <lucsomers@hotmail.com>
2021-03-30 13:22:50 +0200

    Added a tilemanager setup

---
+++ Mercier/Assets/TileManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    #region SingleTon
+    public static TileManager instance;
+
+    private void Awake()
+    {
+        if (instance == null || instance != this)
+        {
+            instance = this;
+        }
+    }
+    #endregion
+
+    private List<SmartTile> tiles = new List<SmartTile>();
+
+    private void Start()
+    {
+        SetupTileList();
+    }
+
+    private void SetupTileList()
+    {
+        foreach (SmartTile tile in transform.GetComponentsInChildren<SmartTile>())
+        {
+
+        }
+    }
+}
