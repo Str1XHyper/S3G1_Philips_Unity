13939f9615be7b74e0d1696239f5ff947ffb8282
Luc Somers <lucsomers@hotmail.com>
2021-04-12 15:15:56 +0200

    Added moneygain and money lose tile functionality

--- Mercier/Assets/Script/Tiles/MoneyGainTile.cs
+++ Mercier/Assets/Script/Tiles/MoneyGainTile.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class MoneyGainTile : SmartTile
 {
+    private const int AmountToGain = 3;
+
     public override void HandleTile(PlayerGroup currentPlayerGroup)
     {
         base.HandleTile(currentPlayerGroup);
+
+        currentPlayerGroup.GiveMoney(AmountToGain);
+
+        Debug.Log("You gained " + AmountToGain + " money you new balance is " + currentPlayerGroup.CurrentMoneyAmount);
     }
 }
--- Mercier/Assets/Script/Tiles/PaymentTile.cs
+++ Mercier/Assets/Script/Tiles/PaymentTile.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PaymentTile : SmartTile
 {
+    private const int PayUpAmount = 3;
+
     public override void HandleTile(PlayerGroup currentPlayerGroup)
     {
         base.HandleTile(currentPlayerGroup);
+
+        int payedAmount = currentPlayerGroup.SubtractMoney(PayUpAmount);
+        Debug.Log("You lost " + payedAmount + " money");
     }
 }
