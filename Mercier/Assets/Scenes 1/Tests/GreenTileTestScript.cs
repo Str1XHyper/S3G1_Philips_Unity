0e6a7665bb63faaf22c6447a8419d193c6f5d6a8
juju125 <justin555@live.nl>
2021-04-19 14:57:22 +0200

    Fixed the assembly issues with the unity test, Only got the issue now that unity doesnt reconigze the unit test

---
+++ Mercier/Assets/Scenes 1/Tests/GreenTileTestScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GreenTileTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GreenTileTestScriptSimplePasses()
+        {
+            PlayerGroup player = new PlayerGroup();
+
+            Assert.AreEqual(0, player.CurrentMoneyAmount, "Players money is not equal to 0");
+
+            player.GiveMoney(3);
+
+            Assert.AreEqual(3, player.CurrentMoneyAmount, "Players money is not equal to 3");
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GreenTileTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
