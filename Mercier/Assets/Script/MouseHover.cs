dcdf93c6fa7d36cc6e1b8fefd456e60bf1bd23c0
juju125 <justin555@live.nl>
2021-05-18 14:07:16 +0200

    Starting on working to add working buttons to the main menu

---
+++ Mercier/Assets/Script/MouseHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+
+    private void OnMouseEnter()
+    {
+       
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+}
