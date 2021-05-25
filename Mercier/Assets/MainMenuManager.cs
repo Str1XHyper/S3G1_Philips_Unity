3423c9f5b9123f2a18dabc1e2a177045ca1250a5
juju125 <justin555@live.nl>
2021-05-25 12:21:37 +0200

    Reworked the Main Menu

---
+++ Mercier/Assets/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
