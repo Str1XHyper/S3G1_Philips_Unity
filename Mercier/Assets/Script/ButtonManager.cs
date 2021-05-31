b3d87907a5e7704f082665be1ef4ae5144fef4cd
juju125 <justin555@live.nl>
2021-05-31 10:50:26 +0200

    Reworked the Main  Menu with a new animation and rework of the canvas

--- Mercier/Assets/Script/ButtonManager.cs
+++ Mercier/Assets/Script/ButtonManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +8,9 @@ public class ButtonManager : MonoBehaviour
 {
     public void LoadNextLevel()
     {
+        Console.WriteLine(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Console.WriteLine(SceneManager.GetActiveScene().name);
     }
 
     public void CloseApplication()
