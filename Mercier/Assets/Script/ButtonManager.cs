8a563689a8c103a8866786f4cedbcf729a00a53f
juju125 <justin555@live.nl>
2021-06-01 11:31:15 +0200

    Started working on the settings page. Added transaction between Main menu and settings

--- Mercier/Assets/Script/ButtonManager.cs
+++ Mercier/Assets/Script/ButtonManager.cs
@@ -6,15 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    private Camera camera;
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = camera.GetComponent<Animator>();
+    }
+
     public void LoadNextLevel()
     {
-        Console.WriteLine(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Console.WriteLine(SceneManager.GetActiveScene().name);
     }
 
     public void CloseApplication()
     {
         Application.Quit();
+    }
+
+    public void LoadSettings()
+    {
+        animator.ResetTrigger("MenuClick");
+        animator.SetTrigger("SettingsClick");
+    }
+
+    public void LoadMenu()
+    {
+        animator.ResetTrigger("SettingsClick");
+        animator.SetTrigger("MenuClick");
     }
 }
